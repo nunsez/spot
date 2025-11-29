8d5c9cdaf927ecd85a994754e3323a1b71c04383
Alexander Mandrikov <mandrikov@pm.me>
2025-11-29 14:17:10 +0700

    remove external metaflac dependency

--- src/set_metadata.rs
+++ src/set_metadata.rs
@@ -1,8 +1,8 @@
+use metaflac::{Tag, block::PictureType};
 use reqwest::blocking;
 use std::{
     fs,
     path::{Path, PathBuf},
-    process::Command,
 };
 
 use crate::{track::Track, utils::Result};
@@ -12,7 +12,6 @@ pub fn call() -> Result<()> {
     let tracks: Vec<Track> = serde_json::from_str(&content)?;
 
     fs::create_dir_all("images")?;
-    fs::create_dir_all("metadata")?;
 
     for track in tracks {
         if let Err(e) = process(&track) {
@@ -25,8 +24,7 @@ pub fn call() -> Result<()> {
 
 fn process(track: &Track) -> Result<()> {
     let image_path = download_image(track).unwrap_or_default();
-    let metadata_path = save_metadata(track)?;
-    let track_path = set_metadata(track, &metadata_path, &image_path)?;
+    let track_path = set_metadata(track, &image_path)?;
 
     fs::rename(&track_path, track_name(track)).map_err(|_| "track rename error")?;
     println!("{}", msg(track, "OK!"));
@@ -49,46 +47,7 @@ fn download_image(track: &Track) -> Result<PathBuf> {
     Ok(path)
 }
 
-fn save_metadata(track: &Track) -> Result<PathBuf> {
-    let mut rows = vec![
-        build_tag_row("TITLE", &track.name),
-        build_tag_row("ARTIST", &track.artists),
-        build_tag_row("ALBUM", &track.album_name),
-        build_tag_row("DATE", &track.album_release_date),
-        build_tag_row("COMMENT", &track.spotify_url),
-    ];
-
-    if let Some(year) = extract_year(&track.album_release_date) {
-        rows.push(build_tag_row("YEAR", &year));
-    }
-
-    if track.track_number != 0 {
-        rows.push(build_tag_row(
-            "TRACKNUMBER",
-            &track.track_number.to_string(),
-        ));
-    }
-
-    if track.total_tracks != 0 {
-        rows.push(build_tag_row(
-            "TOTALTRACKS",
-            &track.total_tracks.to_string(),
-        ));
-    }
-
-    let metadata_path = Path::new("metadata").join(&track.id);
-
-    let content = format!("{}\n", rows.join("\n"));
-    fs::write(&metadata_path, content)?;
-
-    Ok(metadata_path)
-}
-
-fn build_tag_row(key: &str, value: &str) -> String {
-    format!("{}={}", key.to_uppercase(), value)
-}
-
-fn set_metadata(track: &Track, metadata_path: &Path, image_path: &Path) -> Result<PathBuf> {
+fn set_metadata(track: &Track, image_path: &Path) -> Result<PathBuf> {
     let p = format!("{}.flac", &track.id);
     let track_path = Path::new(&p);
 
@@ -96,26 +55,54 @@ fn set_metadata(track: &Track, metadata_path: &Path, image_path: &Path) -> Resul
         return Err(msg(track, "track not found").into());
     }
 
-    let mut binding = Command::new("metaflac");
-    let cmd = binding.arg("--no-utf8-convert").arg("--remove-all-tags");
+    let mut tag = Tag::read_from_path(track_path).map_err(|_| msg(track, "tag reading error"))?;
 
-    if metadata_path.exists() {
-        cmd.arg("--import-tags-from").arg(metadata_path);
-    } else {
-        eprintln!("{}", msg(track, "metadata file not found"));
+    // clear vorbis
+    tag.remove_blocks(metaflac::BlockType::VorbisComment);
+
+    set_vorbis_first(&mut tag, "TITLE", &track.name);
+    set_vorbis_first(&mut tag, "ARTIST", &track.artists);
+    set_vorbis_first(&mut tag, "ALBUM", &track.album_name);
+    set_vorbis_first(&mut tag, "DATE", &track.album_release_date);
+    set_vorbis_first(&mut tag, "COMMENT", &track.spotify_url);
+
+    if let Some(year) = extract_year(&track.album_release_date) {
+        set_vorbis_first(&mut tag, "YEAR", &year);
     }
 
-    if image_path.exists() {
-        cmd.arg("--import-picture-from").arg(image_path);
-    } else {
-        eprintln!("{}", msg(track, "image file not found"));
+    if track.track_number != 0 {
+        let track_number = track.track_number.to_string();
+        set_vorbis_first(&mut tag, "TRACKNUMBER", &track_number);
     }
 
-    cmd.arg(track_path);
+    if track.total_tracks != 0 {
+        let total_tracks = track.total_tracks.to_string();
+        set_vorbis_first(&mut tag, "TOTALTRACKS", &total_tracks);
+    }
 
-    cmd.output().map_err(|_| msg(track, "metaflac error"))?;
+    match fs::read(image_path) {
+        Ok(picture) => match infer::get(&picture) {
+            Some(mime_type) => {
+                let picture_type = PictureType::CoverFront;
+                tag.remove_picture_type(picture_type);
+                tag.add_picture(mime_type.mime_type(), picture_type, picture);
+            }
+            None => {
+                eprintln!("{}", msg(track, "unknown image format"));
+            }
+        },
+        Err(_) => {
+            eprintln!("{}", msg(track, "image file not found"));
+        }
+    }
+
+    tag.save().map_err(|_| msg(track, "tag saving error"))?;
 
     Ok(track_path.to_path_buf())
+}
+
+fn set_vorbis_first(tag: &mut Tag, key: &str, value: &str) {
+    tag.set_vorbis(key, vec![value]);
 }
 
 fn track_name(track: &Track) -> String {
